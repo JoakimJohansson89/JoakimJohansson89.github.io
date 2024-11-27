58f97ba90bda3e0ffc912f0846d4b09bd806294b
JoakimJohansson89 <143935849+JoakimJohansson89@users.noreply.github.com>
2024-11-27 13:17:53 +0100

    Update gandalf.js

--- gandalf.js
+++ gandalf.js
@@ -1,5 +1,5 @@
 document.getElementById("fetch-quote").addEventListener("click", function (){
-    fetch("http://127.0.0.1:5000/quote")
+    fetch("https://joakimjohansson89-github-io-8.onrender.com/quote/quote")
     .then(response => response.json())
     .then(data => {
         // Update the quote in the paragraph
